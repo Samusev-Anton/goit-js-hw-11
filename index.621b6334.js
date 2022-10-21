33f3b438834690c70cfa9b5102a5c48b94f46b5e
Samusev-Anton <104672550+Samusev-Anton@users.noreply.github.com>
2022-10-21 20:08:03 +0000

    Deploying to gh-pages from @ Samusev-Anton/goit-js-hw-11@cef7502bebe7f648969bbcdb1bf75a02a652daeb 🚀

--- index.621b6334.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".search-form");console.log(e);document.querySelector("button");e.addEventListener("submit",(function(e){e.preventDefault(),console.log(e.currentTarget.elements.searchQuery.value);e.currentTarget.elements.searchQuery.value}))}();
-//# sourceMappingURL=index.621b6334.js.map
